0b1eb20ab62ba752c7f9b892328ca59b3afd4e63
Lawrence Lan <lawrence.lan@blackboard.com>
2016-09-23 09:24:06 +0800

    first commit

---
+++ PhoneStats/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PhoneStats
+{
+	[Activity(Label = "Phone Stats", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			button.Click += delegate { 
+				Toast.MakeText(this, button.Text, ToastLength.Short).Show(); 
+			};
+		}
+	}
+}
+
