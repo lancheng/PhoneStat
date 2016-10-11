aa2abb52f03971b62a870ae4bdc4bf13e1254b1f
Lawrence Lan <lawrence.lan@blackboard.com>
2016-10-11 23:22:54 +0800

    add code to get phone state stats

--- PhoneStats/CollectService.cs
+++ PhoneStats/CollectService.cs
@@ -12,21 +12,15 @@ namespace PhoneStats
 	public class CollectService: Service
 	{
 		static readonly string TAG = "X:" + typeof(CollectService).Name;
-		static readonly int TimerWait = 4000;
-		Timer _timer;
 		SMSReceiver smsReceiver = new SMSReceiver();
-		//SMSSender smsSender = new SMSSender();
+		PhoneCallReceiver phoneCallReceiver = new PhoneCallReceiver();
 
 		public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
 		{
 			Log.Debug(TAG, "OnStartCommand called at {2}, flags={0}, startid={1}", flags, startId, DateTime.UtcNow);
-			_timer = new Timer(o => { Log.Debug(TAG, "Hello from SimpleService. {0}", DateTime.UtcNow); },
-							   null,
-							   0,
-							   TimerWait);
 
 			RegisterReceiver(smsReceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
-			//RegisterReceiver(smsSender, new IntentFilter("android.provider.Telephony.SMS_SENT"));
+			RegisterReceiver(phoneCallReceiver, new IntentFilter("android.intent.action.PHONE_STATE"));
 
 			return StartCommandResult.Sticky;
 		}
@@ -35,12 +29,9 @@ namespace PhoneStats
 		{
 			base.OnDestroy();
 
-			_timer.Dispose();
-			_timer = null;
-
 			Log.Debug(TAG, "SimpleService destroyed at {0}.", DateTime.UtcNow);
 			UnregisterReceiver(smsReceiver);
-			//UnregisterReceiver(smsSender);
+			UnregisterReceiver(phoneCallReceiver);
 
 		}
 
--- PhoneStats/StatsReceivers.cs
+++ PhoneStats/StatsReceivers.cs
@@ -2,12 +2,11 @@
 using Android.App;
 using Android.Content;
 using Android.Widget;
-//using Android.Telephony;
+using Android.Telephony;
+using Android.Util;
 
 namespace PhoneStats
 {
-	//[BroadcastReceiver(Label = "SMS Receiver")]
-	//[IntentFilter(new string[] { "android.provider.Telephony.SMS_RECEIVED" })]
 	public class SMSReceiver : Android.Content.BroadcastReceiver
 	{
 		public static readonly string IntentAction = "android.provider.Telephony.SMS_RECEIVED";
@@ -17,9 +16,7 @@ namespace PhoneStats
 			try
 			{
 				if (intent.Action != IntentAction) return;
-				String log_text = String.Format("{0}: 3", DateTime.Now);
-				Toast.MakeText(context, log_text, ToastLength.Long).Show();
-
+				Log.Debug("X:" + typeof(SMSReceiver).Name, "3: Receive a SMS.");
 			}
 			catch (Exception ex)
 			{
@@ -27,6 +24,137 @@ namespace PhoneStats
 			}
 		}
 
+	}
+
+	interface PhoneCallState
+	{
+		PhoneCallState Ring();
+		PhoneCallState Offhook();
+		PhoneCallState Idle();
+	};
+
+	class PhoneCallRing : PhoneCallState
+	{
+		private static PhoneCallRing ringState = null;
+		private PhoneCallRing() { }
+
+		static public PhoneCallState GetInstance()
+		{
+			if (ringState == null)
+			{
+				ringState = new PhoneCallRing();
+			}
+
+			return ringState;
+		}
+
+		public PhoneCallState Ring()
+		{
+			return PhoneCallRing.GetInstance();
+		}
+
+		public PhoneCallState Offhook()
+		{
+			Log.Debug("X:" + typeof(PhoneCallReceiver).Name, "1: Incoming call answered.");
+			return PhoneCallOffhook.GetInstance();
+		}
+
+		public PhoneCallState Idle()
+		{
+			Log.Debug("X:" + typeof(PhoneCallReceiver).Name, "2: Incoming call ended.");
+			return PhoneCallIdle.GetInstance();
+		}
+	}
+
+	class PhoneCallOffhook: PhoneCallState
+	{
+		private static PhoneCallOffhook offhookState = null;
+		private PhoneCallOffhook() { }
+
+		static public PhoneCallState GetInstance()
+		{
+			if (offhookState == null)
+			{
+				offhookState = new PhoneCallOffhook();
+			}
+
+			return offhookState;
+		}
+
+		public PhoneCallState Ring()
+		{
+			Log.Debug("X:" + typeof(PhoneCallReceiver).Name, "Exception");
+			return PhoneCallRing.GetInstance();
+		}
+
+		public PhoneCallState Offhook()
+		{
+			return PhoneCallOffhook.GetInstance();
+		}
+
+		public PhoneCallState Idle()
+		{
+			Log.Debug("X:" + typeof(PhoneCallReceiver).Name, "2: call ended.");
+			return PhoneCallIdle.GetInstance();
+		}
+	}
+
+	class PhoneCallIdle : PhoneCallState
+	{
+		private static PhoneCallIdle idleState = null;
+		private PhoneCallIdle() { }
+
+		static public PhoneCallState GetInstance()
+		{
+			if (idleState == null)
+			{
+				idleState = new PhoneCallIdle();
+			}
+
+			return idleState;
+		}
+
+		public PhoneCallState Ring()
+		{
+			Log.Debug("X:" + typeof(PhoneCallReceiver).Name, "Incoming call.");
+			return PhoneCallRing.GetInstance();
+		}
+
+		public PhoneCallState Offhook()
+		{
+			Log.Debug("X:" + typeof(PhoneCallReceiver).Name, "0: Outgoing call.");
+			return PhoneCallOffhook.GetInstance();
+		}
+
+		public PhoneCallState Idle()
+		{
+			return PhoneCallIdle.GetInstance();
+		}
+	}
+
+	public class PhoneCallReceiver : BroadcastReceiver
+	{
+		private PhoneCallState currentState = PhoneCallIdle.GetInstance();
+
+		public override void OnReceive(Context context, Intent intent)
+		{
+			if (intent.Extras != null)
+			{
+				string state = intent.GetStringExtra(TelephonyManager.ExtraState);
+				if (state == TelephonyManager.ExtraStateRinging)
+				{
+					currentState = currentState.Ring();
+				}
+				else if (state == TelephonyManager.ExtraStateOffhook)
+				{
+					currentState = currentState.Offhook();
+				}
+				else if (state == TelephonyManager.ExtraStateIdle)
+				{
+					currentState = currentState.Idle();
+				}
+			}
+		}
 	}
 
 	/*[BroadcastReceiver(Label = "SMS Sender")]
